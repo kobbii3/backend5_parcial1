7385560627975fd801936330575a09d09574b098
Jair Acevedo <jairjoseacevedomateus123@hotmail.com>
2023-09-19 19:27:33 -0500

    Pagina inicial

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="css\style.css">
+    <title>Parcial1</title>
+</head>
+<body>
+    <h1>Parcial 1 BACKEND</h1>
+
+    <form action="punto4.php" method="post">
+        <input type="submit" value="PUNTO 4 / JAIR ACEVEDO">
+    </form>
+
+    <form action="punto6.php" method="post">
+        <input type="submit" value="PUNTO 6 / CRISTIAN PARADA">
+    </form>
+    
+    <form action="punto2.php" method="post">
+        <input type="submit" value="PUNTO 2 / CARLOS AGUDELO">
+    </form>
+    
+    <form action="punto1.php" method="post">
+        <input type="submit" value="PUNTO 1 / ALIRIO SANCHEZ">
+    </form>
+</body>
+</html>
